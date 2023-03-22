af15503ce4d2ede06eadeba965d470a7873a12a7
mh062 <hasan1907062@stud.kuet.ac.bd>
2023-03-22 12:25:09 +0600

    //

--- test2.java
+++ test2.java
@@ -3,5 +3,11 @@ public class test2 {
     String aa;
     int bb;
 
-    
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
+
+    public void setBb(int bb) {
+        this.bb = bb;
+    }
 }
