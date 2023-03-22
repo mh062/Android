24264a5c864b335fbd1e0b46a3731c2228e0ee58
mh062 <hasan1907062@stud.kuet.ac.bd>
2023-03-22 12:19:12 +0600

    commit test1

---
+++ test1.java
@@ -0,0 +1,26 @@
+public class test1 {
+
+    String aa;
+    int bb;
+
+    public test1(String aa, int bb) {
+        this.aa = aa;
+        this.bb = bb;
+    }
+
+    public String getAa() {
+        return aa;
+    }
+
+    public int getBb() {
+        return bb;
+    }
+
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
+
+    public void setBb(int bb) {
+        this.bb = bb;
+    }
+}
